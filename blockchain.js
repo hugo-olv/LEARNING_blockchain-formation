3b7a34ab76f97843ec88cfac66a447381da0e015
themaskedcat <oliveira.developpement@gmail.com>
2022-03-01 02:05:31 +0100

    Module - 1 complete : Fully functionnal basic blockchain in node.js

--- blockchain.js
+++ blockchain.js
@@ -1,3 +1,65 @@
-class Blockchain {
-    
+const crypto = require('crypto');
+
+module.exports = class Blockchain {
+    constructor(leadingZero) {
+        this.leadingZero = leadingZero
+        this.chain = []
+        this.createBlock(1, '0')
+    }
+
+    createBlock(proof, previousHash) {
+        const block = {
+            index: this.chain.length + 1,
+            timestamp: Date.now().toString(),
+            proof,
+            previousHash
+        }
+
+        this.chain.push(block)
+
+        return block
+    }
+
+    getPreviousBlock() {
+        return this.chain[this.chain.length - 1]
+    }
+
+    proofOfWork(previousProof) {
+        let newProof = 1
+        let checkProof = false
+
+        while (checkProof === false) {
+            const difProof = (newProof ** 2) - (previousProof ** 2)
+            const hashOperation = crypto.createHash('sha256').update(difProof.toString()).digest('hex')
+
+            if (hashOperation.slice(0, this.leadingZero) === '0'.repeat(this.leadingZero)) {
+                checkProof = true
+            }
+            else newProof += 1
+        }
+
+        return newProof
+    }
+
+    hash(block) {
+        const stringifiedBlock = JSON.stringify(block)
+        return crypto.createHash('sha256').update(stringifiedBlock).digest('hex')
+    }
+
+    isValidBlock(chain) {
+        let previousBlock = chain[0]
+
+        chain.forEach((block, idx) => {
+            const difProof = (block.proof ** 2) - (previousBlock.proof ** 2)
+            const hashOperation = crypto.createHash('sha256').update(difProof.toString()).digest('hex')
+
+            if (block.previousHash !== this.hash(previousBlock)) return false
+
+            if (hashOperation.slice(0, this.leadingZero) !== '0'.repeat(this.leadingZero)) return false
+
+            previousBlock = block
+        })
+
+        return true
+    }
 }
--- server.js
+++ server.js
@@ -2,10 +2,39 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const Blockchain = require('./blockchain')
+
+const blockchain = new Blockchain(4)
+
+app.get('/mine_block', (req, res) => {
+    const previousBlock = blockchain.getPreviousBlock()
+    const previousProof = previousBlock.proof
+    const previousHash = blockchain.hash(previousBlock)
+    const proof = blockchain.proofOfWork(previousProof)
+    const block = blockchain.createBlock(proof, previousHash)
+
+    const response = {
+        message: 'Congratulations, you just mined a block!',
+        ...block
+    }
+
+    res.status(200).json(response)
+})
+
+app.get('/get_chain', (req, res) => {
+
+    const response = {
+        //I add the current hash of each block to quickly compare with the previous hash of following blocks inside the response.
+        chain: blockchain.chain.map(block => ({
+            ...block,
+            hash: blockchain.hash(block)
+        })),
+        length: blockchain.chain.length
+    }
+
+    res.status(200).json(response)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Blockchain app listening on port ${port}`)
 })
